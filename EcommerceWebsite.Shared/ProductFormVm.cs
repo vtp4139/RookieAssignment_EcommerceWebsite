58d3fbb45bc7b2cc952a38d2073d9bc03f38fa45
Tuan Phuong <tuanphuong4139@gmail.com>
2021-03-31 16:03:13 +0700

    Add view models to shared

---
+++ EcommerceWebsite.Shared/ProductFormVm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceWebsite.Shared
+{
+    public class ProductFormVm
+    {
+        [Required]
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Images { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public int CategoryID { get; set; }
+
+    }
+}
---
+++ EcommerceWebsite.Shared/ProductVm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceWebsite.Shared
+{
+    public class ProductVm
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Images { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public ICollection<int> OrderDetailIds { get; set; }
+
+    }
+}
